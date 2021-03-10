a7eb90fa2e2bb85615b490b33bc3ae09a7f1f600
rGudavadze <roma.gudavadze@gmail.com>
2021-03-10 14:43:39 +0400

    added files

--- pirple.py
+++ pirple.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("structure.html")
    
+@app.route("/football", methods=['GET'])
+def football():
+    return render_template("football.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-
