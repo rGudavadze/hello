a2af63afd80e30ca7da9c888caf1448355f812d5
rGudavadze <roma.gudavadze@gmail.com>
2021-03-10 19:59:38 +0400

    added forms

--- pirple.py
+++ pirple.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template("structure.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username == "username" and password == "password":
+            return render_template("index.html", message="You're signed in")
+        else:
+            error_message = "Hint: He curses a lot"
+            return render_template("index.html", message=error_message)
    
 @app.route("/football", methods=['GET'])
 def football():
