7013728457a5f36cd50d6eee4d96b950ada12c69
rGudavadze <roma.gudavadze@gmail.com>
2021-03-12 00:30:50 +0400

    sign up and sign in

--- model.py
+++ model.py
@@ -12,3 +12,56 @@ def show_color(username):
 
     message = f"{username}'s favourite color is {color}"
     return message
+
+def check_pw(username):
+    connection = sqlite3.connect("flask_tut.db", check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(f"""SELECT password FROM users WHERE username='{username}' ORDER BY pk DESC;""")
+    password = cursor.fetchone()[0]
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return password
+
+
+def signup(username, password, fav_color):
+    connection = sqlite3.connect("flask_tut.db", check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(f"""SELECT password FROM users WHERE username='{username}';""")
+    exist = cursor.fetchone()
+
+    if exist is None:
+        cursor.execute(f"""INSERT INTO users (username, password, fav_color)
+        VALUES('{username}', '{password}', '{fav_color}');""")
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+    else:
+        return "User already exist!!!"
+    
+    return "You've succesfully signed up"
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def check_users():
+    connection = sqlite3.connect("flask_tut.db", check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute("""SELECT username FROM users ORDER BY pk DESC;""")
+    db_users = cursor.fetchall()
+    users = []
+
+    for i in range(len(db_users)):
+        person = db_users[i][0]
+        users.append(person)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return users
--- pirple.py
+++ pirple.py
@@ -1,25 +1,61 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, g
 import model
 
 app = Flask(__name__)
+app.secret_key = 'jumpjacks'
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-    if request.method == 'GET':
-        return render_template("index.html")
+username = ''
+user = model.check_users()
+
+
+@app.route("/", methods=['GET'])
+def home():
+    if 'username' in session:
+        g.user = session['username']
+        return render_template('football.html', message="<img src='static/img/unnamed.png'>")
+    return render_template('homepage.html', message="Log in or sign up!")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        session.pop("username", None)
+        areyouuser = request.form.get('username')
+        pwd = model.check_pw(areyouuser)
+        if request.form.get('password') == pwd:
+            session['username'] = request.form.get('username')
+            return redirect(url_for('home'))
+    
+    return render_template('index.html')
+
+@app.before_request
+def before_request():
+    g.username = None
+    if 'username' in session:
+        g.username = session['username']
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method=='GET':
+        message = "Please sign up!"
+        return render_template("signup.html", message=message)
     else:
         username = request.form.get("username")
         password = request.form.get("password")
-        if username == "jaytatum" and password == "boston":
-            message = model.show_color("jaytatum")
-            return render_template("football.html", message=message)
-        else:
-            error_message = "Hint: He curses a lot"
-            return render_template("index.html", message=error_message)
-   
-@app.route("/football", methods=['GET'])
-def football():
-    return render_template("football.html")
+        fav_color = request.form.get("fav_color")
+        message = model.signup(username, password, fav_color)
+        return render_template('signup.html', message=message)
+
+@app.route('/getsession')
+def getsession():
+    if 'username' in session:
+        return session['username']
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
